f866becbc0418a47331f402232817f3448efd32c
Orhun Eren Yalçınkaya <orhun868@gmail.com>
2024-08-03 17:20:07 +0300

    Update Unity 3D Hand

---
+++ 3D Hand Control/Assets/Scripts/HandTrackerr.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandTrackerr : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public UDPReceive udpReceive;
+    public GameObject[] handPoints;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
